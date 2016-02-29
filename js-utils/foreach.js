27c8dfd4443e500a2ae9d0ea54ca48287e2b07d9
Lilith <lilith.rises.24@gmail.com>
2016-02-29 15:59:12 +0000

    Created foreach function
    General JS utils go in their own folder

---
+++ js-utils/foreach.js
@@ -0,0 +1,13 @@
+// Callback signature: element, index, whole, isArray
+// When the target is neither an array nor an object:
+// element === whole && index === -1 && isArray === false
+function foreach(target, callback) {
+	'use strict';
+	if (typeof target != 'object') {
+		callback(target, -1, target, false);
+	}
+	else {
+		let arrayp = Object.prototype.toString.call(target) == '[object Array]';
+		for (let index in target) callback(target[index], index, target, arrayp);
+	}
+}
